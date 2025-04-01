0a46238c0c4cbd1acdff5686c67f18513e0053ae
Jothir Sailesh <143761882+Jothir-Sailesh@users.noreply.github.com>
2025-04-01 18:17:00 +0530

    Update LinkedListAddition.java

--- Reverse LinkedList Sum/LinkedListAddition.java
+++ Reverse LinkedList Sum/LinkedListAddition.java
@@ -1,14 +1,12 @@
 public class LinkedListAddition {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-
+        //This Logic can solve for any Input of any size 
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
         int carry = 0;
 
-        
         while (l1 != null || l2 != null || carry > 0) {
-
             int sum = carry;
 
             if (l1 != null) {
@@ -29,7 +27,6 @@ public class LinkedListAddition {
         return dummy.next;
     }
 
-
     // Helper method to print the list (for testing) - Given in the SOP
     public static void printList(ListNode node) {
         while (node != null) {
@@ -39,11 +36,9 @@ public class LinkedListAddition {
         System.out.println();
     }
 
-
     //Given code in the SOP
     public static void main(String[] args) {
-
-        //Test case
+        //Test case 
         ListNode l1 = new ListNode(2);
         l1.next = new ListNode(4);
         l1.next.next = new ListNode(3);
@@ -54,11 +49,20 @@ public class LinkedListAddition {
 
         LinkedListAddition solution = new LinkedListAddition();
         printList(solution.addTwoNumbers(l1, l2)); 
+
+        // Here I added an Extra Test case for 2 Digit numbers, Since the Logic(addTwoNumbers Method) can pass any Testcase.
+        ListNode l3 = new ListNode(6);
+        l3.next = new ListNode(9);
+
+        ListNode l4 = new ListNode(9);
+        l4.next = new ListNode(6);
+
+        printList(solution.addTwoNumbers(l3, l4)); // Expected Output: 5 6 1 (Represents 165)
     }
 }
 
-
-
 /*  Program Output:  7 0 8     (For the Given Test Case)
     Expected Output: 7 0 8     (For the Given Test Case)
+
+    Additional Test Case Output: 5 6 1  (For 69 + 96 = 165)
 */
